5cbf72ec7b685955035c37ee64e2637ab6930ac6
Rao Adnan <41448827+raoadi007@users.noreply.github.com>
2019-08-19 09:00:38 +0500

    Create osmApi.py
    This is a OpenStreetMap Api using Python. In this file, we have used some packages like urlopen, time, exc_info and xmltodict. We fetch latitudes, longitudes, Address, Street Name, Postal Code of that Area, Population and Max Speed by OSM ID.

---
+++ osmApi.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from time import sleep
+from sys import exc_info
+from xmltodict import parse
+ 
+ids = [251165455]
+for wayid in ids:
+    try:
+        response = urlopen('http://www.openstreetmap.org/api/0.6/way/{0}/full'.format(wayid))
+        s = response.read()
+        d = parse(s)
+        way_nodes_coord = d['osm']['node']
+        for j in way_nodes_coord:
+            print("lat = {0}, lon = {1}".format(float(j['@lat']),float(j['@lon'])))
+        for tag in d['osm']['way']['tag']:
+            print("key = {0}, value = {1}".format(tag['@k'], tag['@v']))
+        sleep(1)
+    except:
+        print("Got error for location {0}".format(wayid))
